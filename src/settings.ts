23a1e4bbc3061171806e913dbebc2c183ff79e30
Nick Greenlees <greenleesnick@gmail.com>
2020-10-05 20:56:33 -0600

    Moving min and max setting to input restrictions obj

--- src/settings.ts
+++ src/settings.ts
@@ -5,10 +5,12 @@ export const DEFAULT_STATE_DIMS = {
   width: 200,
   height: 300,
 };
+export const DEFAULT_URL = `${BASE_URL}/${DEFAULT_STATE_DIMS.width}/${DEFAULT_STATE_DIMS.height}`;
 export const DEFAULT_STATE_ASPECT = {
   ratio: round(DEFAULT_STATE_DIMS.width / DEFAULT_STATE_DIMS.height, 2),
   isLocked: true,
 };
-export const DEFAULT_URL = `${BASE_URL}/${DEFAULT_STATE_DIMS.width}/${DEFAULT_STATE_DIMS.height}`;
-export const INPUT_MIN = 25;
-export const INPUT_MAX = 3000;
+export const INPUT_RESTRICTIONS = {
+  MIN: 50,
+  MAX: 2500,
+};
